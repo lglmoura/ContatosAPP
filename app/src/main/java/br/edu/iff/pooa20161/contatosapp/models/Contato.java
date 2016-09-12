a8b8b31d4ec619fe229a9486308ec8c3688c126f
Luiz Gustavo <lglmoura@gmail.com>
2016-09-12 18:05:10 -0300

    Criando a Classe de Modelo Contato

---
+++ app/src/main/java/br/edu/iff/pooa20161/contatosapp/models/Contato.java
@@ -0,0 +1,49 @@
+package br.edu.iff.pooa20161.contatosapp.models;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by lglmoura on 12/09/16.
+ */
+public class Contato extends SugarRecord {
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+
+    public Contato() {
+
+    }
+    public Contato( String nome, String email, String telefone) {
+
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
